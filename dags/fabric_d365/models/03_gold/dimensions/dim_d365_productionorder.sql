4950b669f4eb55d904fce4188e96aa38da623e6d
Edgar Taboada <etaboada@sanford.co.nz>
2024-08-29 15:54:15 +1200

    test fixes

--- dags/fabric_d365/models/03_gold/dimensions/dim_d365_productionorder.sql
+++ dags/fabric_d365/models/03_gold/dimensions/dim_d365_productionorder.sql
@@ -9,10 +9,9 @@ select
     , pt.prodid
     , pt.dxc_parentprodid as parentprodid
     , pt.prodpoolid
-    , eppt.[LocalizedLabel] as prodpostingtype
-    , {{ translate_enum('eppt', 'pt.prodpostingtype' ) }} as prodpostingtype
-    , {{ translate_enum('eps', 'pt.prodstatus' ) }} as prodstatus
-    , {{ translate_enum('epbs', 'pt.backorderstatus' ) }} as backorderstatus
+    , {{ translate_enum('eppt', 'pt.prodpostingtype') }} as prodpostingtype
+    , {{ translate_enum('eps', 'pt.prodstatus') }} as prodstatus
+    , {{ translate_enum('epbs', 'pt.backorderstatus') }} as backorderstatus
     , pt.dxc_salesid as salesid
     , pt.dxc_salesinventtransid as salesinventtransid
     , sl.qtyordered as salesline_qtyordered
@@ -37,22 +36,22 @@ select
     , case when pt.dxc_saleslinenumber = 0 then null else pt.dxc_saleslinenumber end as saleslinenumber
     , upper(sl.salesunit) as salesline_unit
     , round(case when upper(sl.salesunit) = 'LB' then sl.qtyordered * 0.45359237 else sl.qtyordered end, 3) as salesline_qtyordered_kg
-    , cast (pt.bomdate as date) as bomdate
-    , cast (pt.calcdate as date) as calcdate
-    , case when cast (pt.dlvdate as date) = '1900-01-01' then null
-        else cast (concat(convert(varchar(10), cast (pt.dlvdate as date), 120), ' ', convert(varchar(8), dateadd(second, pt.dlvtime, 0), 108)) as datetime2(0))
+    , cast(pt.bomdate as date) as bomdate
+    , cast(pt.calcdate as date) as calcdate
+    , case when cast(pt.dlvdate as date) = '1900-01-01' then null
+        else cast(concat(convert(varchar(10), cast(pt.dlvdate as date), 120), ' ', convert(varchar(8), dateadd(second, pt.dlvtime, 0), 108)) as datetime2(0))
     end as deliverydatetime
-    , case when cast (pt.finisheddate as date) = '1900-01-01' then null else cast (pt.finisheddate as date) end as finisheddate
-    , cast (concat(convert(varchar(10), cast (pt.latestscheddate as date), 120), ' ', convert(varchar(8), dateadd(second, pt.latestschedtime, 0), 108)) as datetime2(0)) as latestscheduled_datetime
-    , case when cast (pt.scheddate as date) = '1900-01-01' then null else cast (pt.scheddate as date) end as scheddate
-    , case when cast (pt.schedend as date) = '1900-01-01' then null else cast (pt.schedend as date) end as schedenddate
-    , case when cast (pt.schedstart as date) = '1900-01-01' then null else cast (pt.schedstart as date) end as schedstartdate
-    , cast (dateadd(second, pt.schedfromtime, 0) as time(0)) as schedfromtime
-    , cast (dateadd(second, pt.schedtotime, 0) as time(0)) as schedtotime
-    , case when cast (pt.realdate as date) = '1900-01-01' then null else cast (pt.realdate as date) end as realdate
-    , case when cast (pt.releaseddate as date) = '1900-01-01' then null else cast (pt.releaseddate as date) end as releaseddate
-    , case when cast (pt.dlvdate as date) > '1900-01-01' then cast (pt.dlvdate as date)
-        else cast (pt.releaseddate as date)
+    , case when cast(pt.finisheddate as date) = '1900-01-01' then null else cast(pt.finisheddate as date) end as finisheddate
+    , cast(concat(convert(varchar(10), cast(pt.latestscheddate as date), 120), ' ', convert(varchar(8), dateadd(second, pt.latestschedtime, 0), 108)) as datetime2(0)) as latestscheduled_datetime
+    , case when cast(pt.scheddate as date) = '1900-01-01' then null else cast(pt.scheddate as date) end as scheddate
+    , case when cast(pt.schedend as date) = '1900-01-01' then null else cast(pt.schedend as date) end as schedenddate
+    , case when cast(pt.schedstart as date) = '1900-01-01' then null else cast(pt.schedstart as date) end as schedstartdate
+    , cast(dateadd(second, pt.schedfromtime, 0) as time(0)) as schedfromtime
+    , cast(dateadd(second, pt.schedtotime, 0) as time(0)) as schedtotime
+    , case when cast(pt.realdate as date) = '1900-01-01' then null else cast(pt.realdate as date) end as realdate
+    , case when cast(pt.releaseddate as date) = '1900-01-01' then null else cast(pt.releaseddate as date) end as releaseddate
+    , case when cast(pt.dlvdate as date) > '1900-01-01' then cast(pt.dlvdate as date)
+        else cast(pt.releaseddate as date)
     end as reportdate
     , coalesce(eirt.[LocalizedLabel], 'None') as inventreftype
     , upper(pt.dataareaid) as prod_dataareaid
